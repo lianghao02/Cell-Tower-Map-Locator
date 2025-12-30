67ecb3ce6f21b445375add161f6a19b2304bd43b
ANDY <tpc0941101090@gmail.com>
2025-12-30 13:19:13 +0800

    Refactor: 重構專案結構，將原始碼移至 src 目錄並更新建置設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js,html}"],
   theme: {
     extend: {
       fontFamily: {
